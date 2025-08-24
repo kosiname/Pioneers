73fce78eeb47fe7c12a9d870b409e6edeba84724
Oduah Ikechukwu <kosiname@Kosiname.local>
2025-08-24 16:57:53 +0100

    Made a couple of changes to fix my backend not connecting with Choreo

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from './constants';
 
-const apiURL = '/choreo-apis/pioneers/pioneersapp/v1'
+const apiUrl = '/choreo-apis/pioneers/pioneersapp/v1'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,  
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,  
 });
 
 api.interceptors.request.use((config) => {
